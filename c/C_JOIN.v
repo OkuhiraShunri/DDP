a0e5b9cc2b91208062690525d91b5bc0f86b9ea6
shunriokuhira <shunriaoping99@gmail.com>
2024-05-30 13:45:11 +0900

    2nd commit
    信号名の変更

--- c/C_JOIN.v
+++ c/C_JOIN.v
@@ -4,18 +4,18 @@ module C_JOIN(
 );
 
 //wire send_in, ack_in, mr, ack_out, send_out, cp;
-wire send_0, send_1, send_2, send_3, send_4, send_5, send_6, send_7;
-wire ack_0, ack_1, ack_2, ack_3, ack_4, ack_5, ack_6, ack_7;
+wire send_out1, send_out2, send_out3, send_out4, send_out5, send_out6, send_out7, send_out8, send_out8_and;
+wire ack_out1, ack_out2, ack_out3, ack_out4, ack_out5, ack_out6, ack_out7, ack_out8;
+assign send_out8_and = Send_in & send_out8;
 
-
-C c1(.Send_in(send_0), .Ack_in(ack_1), .MR(), .Ack_out(ack_0), .Send_out(send_1), .CP(CP1));
-C c2(.Send_in(send_1), .Ack_in(ack_2), .MR(), .Ack_out(ack_1), .Send_out(send_2), .CP(CP2));
-C c3(.Send_in(send_2), .Ack_in(ack_3), .MR(), .Ack_out(ack_2), .Send_out(send_3), .CP(CP3));
-C c4(.Send_in(send_3), .Ack_in(ack_4), .MR(), .Ack_out(ack_3), .Send_out(send_4), .CP(CP4));
-C c5(.Send_in(send_4), .Ack_in(ack_5), .MR(), .Ack_out(ack_4), .Send_out(send_5), .CP(CP5));
-C c6(.Send_in(send_5), .Ack_in(ack_6), .MR(), .Ack_out(ack_5), .Send_out(send_6), .CP(CP6));
-C c7(.Send_in(send_6), .Ack_in(ack_7), .MR(), .Ack_out(ack_6), .Send_out(send_7), .CP(CP7));
-C c8(.Send_in(send_7), .Ack_in(ack_0), .MR(), .Ack_out(ack_7), .Send_out(send_0), .CP(CP8));
+C c1(.Send_in(send_out8_and), .Ack_in(ack_out2), .MR(), .Ack_out(ack_out1), .Send_out(send_out1), .CP(CP1));
+C c2(.Send_in(send_out1), .Ack_in(ack_out3), .MR(), .Ack_out(ack_out2), .Send_out(send_out2), .CP(CP2));
+C c3(.Send_in(send_out2), .Ack_in(ack_out4), .MR(), .Ack_out(ack_out3), .Send_out(send_out3), .CP(CP3));
+C c4(.Send_in(send_out3), .Ack_in(ack_out5), .MR(), .Ack_out(ack_out4), .Send_out(send_out4), .CP(CP4));
+C c5(.Send_in(send_out4), .Ack_in(ack_out6), .MR(), .Ack_out(ack_out5), .Send_out(send_out5), .CP(CP5));
+C c6(.Send_in(send_out5), .Ack_in(ack_out7), .MR(), .Ack_out(ack_out6), .Send_out(send_out6), .CP(CP6));
+C c7(.Send_in(send_out6), .Ack_in(ack_out8), .MR(), .Ack_out(ack_out7), .Send_out(send_out7), .CP(CP7));
+C c8(.Send_in(send_out7), .Ack_in(ack_out1), .MR(), .Ack_out(ack_out8), .Send_out(send_out8), .CP(CP8));
 
 
 
