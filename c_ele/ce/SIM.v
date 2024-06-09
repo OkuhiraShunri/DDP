a63fbe9f01ff0c1e3995df4ccbdd4cbd6a7388cc
OKUHIRA Shunri <114981884+OkuhiraShunri@users.noreply.github.com>
2024-06-09 15:19:16 +0900

    CE回路シミュレーション2
    後段にC素子を繋げたところ、EXB信号の値に合わせてハンドシェイクを制御できたぽい

--- c_ele/ce/SIM.v
+++ c_ele/ce/SIM.v
@@ -1,36 +1,21 @@
 `timescale 1ns / 100ps
 module SIM();
-reg ce_send_in, ce_ack_in, mr, exb;
-wire ce_cp, ce_send_out, ce_ack_out;
+reg ce_send_in, mr, exb, c_ack_in;
+wire ce_cp, ce_send_out, ce_ack_out, ce_ack_in, c_send_out, c_cp;
 
 CE ce(.CE_Send_in(ce_send_in), .CE_Ack_in(ce_ack_in), .MR(mr), .Exb(exb), .CE_Ack_out(ce_ack_out), .CE_Send_out(ce_send_out), .CE_CP(ce_cp));
-// initial begin
-//     send_in = 1;
-// #11
-//     send_in = 0;
-// 
+C c(.Send_in(ce_send_out), .Ack_in(c_ack_in), .MR(mr), .Ack_out(ce_ack_in), .Send_out(c_send_out), .CP(c_cp));
+
 initial begin
     ce_send_in = 1;
-    ce_ack_in = 1;
-    exb = 1;
+    c_ack_in = 1;
+    //ce_ack_in = 1;
+    exb = 0;
     mr = 1;
 #100 mr = 0;
 #11  ce_send_in = 0;
 #11  ce_send_in = 1;
-#11   ce_ack_in = 0;
-
-
-
-// #11 send_in = 0;
-// #11 send_in = 1;
-
-//#50 mr = 1;
-// #30  send_in = 0;
-// #30  send_in = 1;
-
-// #5  send_in = 0;
-// #5  send_in = 1;
-
-
+//#11   ce_ack_in = 0;
 end
+
 endmodule
